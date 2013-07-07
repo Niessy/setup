3f3ddf5830c1e1920aae4b8da65a587fc940e033
Dominique Luna <dluna132@gmail.com>
2013-07-07 15:34:11 -0400

    modified README, setup.sh

--- setup.sh
+++ setup.sh
@@ -34,10 +34,16 @@ fi
 if [ -d .emacs.d/ ]; then
     mv .emacs.d .emacs.d~
 fi
-git clone https://github.com/startup-class/dotfiles.git
+if [ -d .vim ]; then
+    mv .vim .vim~
+fi
+
+git clone https://github.com/Niessy/dotfiles.git
 ln -sb dotfiles/.screenrc .
 ln -sb dotfiles/.bash_profile .
 ln -sb dotfiles/.bashrc .
 ln -sb dotfiles/.bashrc_custom .
 ln -sf dotfiles/.emacs.d .
+ln -s  dotfiles/.vim .
+ln -s  dotfiles/.vimrc .
 
